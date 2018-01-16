321393f0ae0d73b06ff0311a63a7bc00ea6aff30
Yuka Maeno <yuka_imato@yahoo.co.jp>
2018-01-16 04:27:17 +0000

    rakuten api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,10 +17,10 @@ class UsersController < ApplicationController
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
-  　end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+end
