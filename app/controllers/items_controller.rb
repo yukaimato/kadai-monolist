dd9c2f429a78fe59e4936add1c5e982bcd837668
Yuka Maeno <yuka_imato@yahoo.co.jp>
2018-01-16 22:19:03 +0000

    want

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,11 @@
 class ItemsController < ApplicationController
   before_action :require_user_logged_in
-
+　
   def new
     @items = []
 
     @keyword = params[:keyword]
-    if @keyword.present? # 
+    if @keyword
       results = RakutenWebService::Ichiba::Item.search({
         keyword: @keyword,
         imageFlag: 1,
@@ -17,6 +17,11 @@ class ItemsController < ApplicationController
         @items << item
       end
     end
+  end
+
+　def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
   end
 
   private
