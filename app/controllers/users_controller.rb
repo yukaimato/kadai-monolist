a2be2e229a2407b4d773cc67bab30b29c2b87f08
Yuka Maeno <yuka_imato@yahoo.co.jp>
2018-01-17 11:36:05 +0000

    b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @items = @user.items.uniq
     @count_want = @user.want_items.count
+    @count_have = @user.have_items.count
    end
 
    def new
@@ -28,4 +29,4 @@ class UsersController < ApplicationController
    def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
    end
-end   
+end
