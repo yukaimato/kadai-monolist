a645c23e7746ca8ac38cf920c665277fa14d1461
Yuka Maeno <yuka_imato@yahoo.co.jp>
2018-01-19 07:45:27 +0000

    m

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,5 +9,5 @@ class Item < ApplicationRecord
   has_many :wants
   has_many :want_users, through: :wants, class_name: 'User', source: :user
   has_many :haves, class_name: 'Have'
-  has_many :want_users, through: :haves, class_name: 'User', source: :user
+  has_many :have_users, through: :haves, class_name: 'User', source: :user
 end
